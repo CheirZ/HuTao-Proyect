d8c3fa449ca18ef9212c9c8a0b03de71b8dca493
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2024-10-05 00:13:57 -0600

    HuTao-Proyect

--- koyeb.js
+++ koyeb.js
@@ -5,11 +5,11 @@ sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-rel
 sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
 sudo dnf clean all -y
 
-RUN git clone https://github.com/David-Chian/Megumin-Bot-MD
+RUN git clone https://github.com/CheirZ/HuTao-Proyect
 
-WORKDIR /root/MeguminBot-MD
+WORKDIR /root/HuTao-Proyect 
 
-COPY ./root/MeguminBot-MD
+COPY ./root/HuTao-Proyect 
 
 RUN yarn install
 
