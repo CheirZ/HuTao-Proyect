18f9f928da1556d65a8d58e1bf411ba1a15dee51
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2024-10-29 17:07:08 -0600

    🦋 HuTao-Proyect ❤️‍🔥

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -2,7 +2,7 @@ var handler = async (m, { conn, args }) => {
 
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-conn.reply(m.chat, '> toma el link de esta chingadera de grupo\n' + link, m, rcanal, { detectLink: true })
+conn.reply(m.chat, '> aqui tienes el link del grupo :3\n\n' + link, m, rcanal, { detectLink: true })
 
 }
 handler.help = ['link']
