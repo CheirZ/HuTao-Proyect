b800cf90e7ab01ab045ef1c9a9d20baef5c44028
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2024-10-29 18:42:47 -0600

    🦋 HuTao-Proyect ❤️‍🔥

--- plugins/descargas-mediafire.js
+++ plugins/descargas-mediafire.js
@@ -1,51 +1,38 @@
-import { mediafiredl } from '@bochilteam/scraper'
 import axios from 'axios'
 import fetch from 'node-fetch'
-let handler = async (m, { conn, args, usedPrefix, command }) => { 
-if (!args[0]) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝚅𝙰𝙻𝙸𝙳𝙾 𝙳𝙴 𝙼𝙴𝙳𝙸𝙰𝙵𝙸𝚁𝙴*`
-try {
-await m.reply(`*⏳ 𝙴𝚂𝙿𝙴𝚁𝙴 𝙴𝙽 𝙻𝙾 𝚀𝚄𝙴 𝙴𝙽𝚅𝙸𝙾 𝚂𝚄 𝙰𝚁𝙲𝙷𝙸𝚅𝙾. . . .* `)
-await m.reply(`⌛ _Cargando..._\n▰▰▰▱▱▱▱▱▱`)
-let json = await mediafireDl2(args[0])
-let caption = `𝙈𝙀𝘿𝙄𝘼𝙁𝙄𝙍𝙀
-*❖ Name* : ${json[0].nama.replace('+', ' ')}
-*❖ Peso* : ${json[0].size}
-*❖ Tipo:* ${json[0].mime.toUpperCase()}`.trim()   
-conn.sendMessage(m.chat, { text: caption, footer: wm }, { quoted: m })
-conn.sendMessage(m.chat, { document : { url: json[0].link }, fileName : json[0].nama.replace('+', ' '), mimetype: json[0].mime.toUpperCase() }, { quoted: m })
-} catch {  
+import cheerio from 'cheerio'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) throw `🧧 Ingrese un enlace de mediafire.\n*Ejemplo:* ${usedPrefix}mediafire https://www.mediafire.com/file/nb63btgjr0dsn4z/COMDELIGHT_v1.1.apk/file`
 try {  
-let res = await mediafiredl(args[0])
-let { url, url2, filename, ext, aploud, filesize, filesizeH } = await res
-let caption = `𝙈𝙀𝘿𝙄𝘼𝙁𝙄𝙍𝙀
-*❖ Name* : ${filename}
-*❖ Peso* : ${filesizeH}
-*❖ Tipo:* ${ext}`.trim()
-let msg = m.reply(caption)
-if (!msg || msg == '') return m.reply(global.wait)
-let res2 = await fetch(`https://api.lolhuman.xyz/api/mediafire?apikey=${lolkeysapi}&url=${args[0]}`)
-let res3 = await res2.json()
-let res4 = await res3.result.link  
-if (!url || url == '') url = res4
-await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
-} catch {  
-await m.reply('*❌ʟᴏ sᴇɴᴛɪᴍᴏs sᴇ ʜᴀ ɢᴇɴᴇʀᴀᴅᴏ ᴜɴ ᴇʀʀᴏʀ ᴠᴜᴇʟᴠᴇ ɪɴᴛᴇɴᴛᴀʀ❌*')
-}}}
+let res = await mediafireDl(args[0])
+let { name, size, date, mime, link } = res
+let caption = `*Nombre:* ${name}
+*Peso:* ${size}
+*Tipo:* ${mime}`.trim()
+conn.reply(m.chat, caption, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: wm, body: 'El Bot mas explosivo de WhatsApp', previewType: 0, thumbnail: null, sourceUrl: md}}})
+await conn.sendFile(m.chat, link, name, '', m, null, { mimetype: mime, asDocument: true })
+} catch (e) {
+await conn.reply(m.chat, `Error, pruebe con ${usedPrefix + command}2`)
+console.log(e)
+handler.limit = false      
+}}
 handler.help = ['mediafire'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.command = /^(mediafire|mediafiredl|dlmediafire)$/i
-handler.register = false;
-handler.group = true;
+handler.tags = ['descargas']
+handler.command = /^(mediafire|mdfire|mf)$/i
+handler.register = true
+handler.group = true
 export default handler
-async function mediafireDl2(url) {
-const res = await axios.get(url) 
+
+async function mediafireDl(url) {
+const res = await axios.get(`https://www-mediafire-com.translate.goog/${url.replace('https://www.mediafire.com/','')}?_x_tr_sl=en&_x_tr_tl=fr&_x_tr_hl=en&_x_tr_pto=wapp`)
 const $ = cheerio.load(res.data)
-const hasil = []
-const link = $('a#downloadButton').attr('href')
-const size = $('a#downloadButton').text().replace('Download', '').replace('(', '').replace(')', '').replace('\n', '').replace('\n', '').replace('', '')
-const seplit = link.split('/')
-const nama = seplit[5]
-let mime = nama.split('.')
-mime = mime[1]
-hasil.push({ nama, mime, size, link })
-return hasil }     
+const link = $('#downloadButton').attr('href')
+const name = $('body > main > div.content > div.center > div > div.dl-btn-cont > div.dl-btn-labelWrap > div.promoDownloadName.notranslate > div').attr('title').replaceAll(' ','').replaceAll('\n','')
+const date = $('body > main > div.content > div.center > div > div.dl-info > ul > li:nth-child(2) > span').text()
+const size = $('#downloadButton').text().replace('Download', '').replace('(', '').replace(')', '').replace('\n', '').replace('\n', '').replace('                         ', '').replaceAll(' ','')
+let mime = ''
+let rese = await axios.head(link)
+mime = rese.headers['content-type']
+return { name, size, date, mime, link }
+}
