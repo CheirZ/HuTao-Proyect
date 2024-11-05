597317e49c5e768057d8242549df0ae30a823330
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2024-11-04 19:47:16 -0600

    🦋 HuTao-Proyect ❤️‍🔥

--- plugins/jadibot-token.js
+++ plugins/jadibot-token.js
@@ -1,7 +1,7 @@
 import fs from "fs"
 async function handler(m, {usedPrefix}) {
     const user = m.sender.split("@")[0]
-    if (fs.existsSync("./jadibot/" + user + "/creds.json")) {
+    if (fs.existsSync("./jadibots/" + user + "/creds.json")) {
         let token = Buffer.from(fs.readFileSync("./jadibots/" + user + "/creds.json"), "utf-8").toString("base64")
         await conn.reply(m.chat, `El token te permite iniciar sesion en otros bots, recomendamos no compartirlo con nadie.\n\n*Tu token es:*`, m, rcanal)
         await conn.reply(m.chat, token, m, rcanal)
