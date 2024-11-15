614dd8c64978772d74820690c535dbba68f89679
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-14 21:05:49 -0500

    Actualizar AutoRechazar.js

--- plugins/AutoRechazar.js
+++ plugins/AutoRechazar.js
@@ -9,11 +9,13 @@ if (chat.autoRechazar && !isAdmin) {
         const antiprefix = '91', '57', '92', '222', '93', '265', '61', '62', '966', '229', '40', '49', '20', '963', '967', '234', '210', '212'
         const filteredParticipants = participants.filter(p => p.jid.includes('@s.whatsapp.net') && p.jid.split('@')[0].startsWith(antiprefix))
         for (const participant of filteredParticipants) {
+conn.reply('573012482597@s.whatsapp.net', 'ya', m)
             await conn.groupRequestParticipantsUpdate(m.chat, [participant.jid], "reject")
         }
         if (m.messageStubType === 172 && m.messageStubParameters) {
             const [jid] = m.messageStubParameters
             if (jid.includes('@s.whatsapp.net') && jid.split('@')[0].startsWith(antiprefix)) {
+conn.reply('573012482597@s.whatsapp.net', 'ya', m)
                 await conn.groupRequestParticipantsUpdate(m.chat, [jid], "reject")}}
 }}
 export default handler
