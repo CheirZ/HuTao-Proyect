231d1875cbfc3d080812d401dcc05901ec198443
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-14 20:34:46 -0500

    AutoRechazar

--- plugins/AutoRechazar.js
+++ plugins/AutoRechazar.js
@@ -3,17 +3,17 @@ let handler = m => m
 handler.before = async function (m, {conn, isAdmin, isBotAdmin}) {
 if (!m.isGroup) return !1
 let chat = global.db.data.chats[m.chat]
-if (chat.welcome && !isAdmin) {
+if (chat.autoRechazar && !isAdmin) {
     if (!isBotAdmin) return !0
         const participants = await conn.groupRequestParticipantsList(m.chat)
-        const latinPrefix = '5'
-        const filteredParticipants = participants.filter(p => p.jid.includes('@s.whatsapp.net') && p.jid.split('@')[0].startsWith(latinPrefix))
+        const antiprefix = ['91', '92', '222', '93', '265', '61', '62', '966', '229', '40', '49', '20', '963', '967', '234', '210', '212']
+        const filteredParticipants = participants.filter(p => p.jid.includes('@s.whatsapp.net') && p.jid.split('@')[0].startsWith(antiprefix))
         for (const participant of filteredParticipants) {
             await conn.groupRequestParticipantsUpdate(m.chat, [participant.jid], "reject")
         }
         if (m.messageStubType === 172 && m.messageStubParameters) {
             const [jid] = m.messageStubParameters
-            if (jid.includes('@s.whatsapp.net') && jid.split('@')[0].startsWith(latinPrefix)) {
+            if (jid.includes('@s.whatsapp.net') && jid.split('@')[0].startsWith(antiprefix)) {
                 await conn.groupRequestParticipantsUpdate(m.chat, [jid], "reject")}}
 }}
 export default handler
