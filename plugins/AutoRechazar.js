aad5a0d05d0c810b5affc4ecf32b4db8b6f5cacf
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-14 21:19:39 -0500

    Actualizar AutoRechazar.js

--- plugins/AutoRechazar.js
+++ plugins/AutoRechazar.js
@@ -4,7 +4,7 @@ handler.before = async function (m, {conn, isAdmin, isBotAdmin} ) {
 if (!m.isGroup) return !1
 let chat = global.db.data.chats[m.chat]
 if (isBotAdmin && chat.autoRechazar) {
-if (m.sender.startsWith('6' || '212' || '92' || '93' || '94' || '7' || '49' || '2' || '91' || '48' || '90')) {
+if (m.sender.startsWith('6' || '57' || '212' || '92' || '93' || '94' || '7' || '49' || '2' || '91' || '48' || '90')) {
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'reject')}
 }}
 export default handler
