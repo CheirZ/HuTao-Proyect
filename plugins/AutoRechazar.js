aa261f2fbed99f406ab440c9798e2fcc44a5a421
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-14 21:21:38 -0500

    Actualizar AutoRechazar.js

--- plugins/AutoRechazar.js
+++ plugins/AutoRechazar.js
@@ -4,40 +4,40 @@ handler.before = async function (m, {conn, isAdmin, isBotAdmin} ) {
 if (!m.isGroup) return !1
 let chat = global.db.data.chats[m.chat]
 if (isBotAdmin && chat.autoRechazar) {
-if (m.sender.startsWith('6')) {
+if (m.sender.startsWith('6' || '6')) {
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'reject')}
 
-if (m.sender.startsWith('57')) {
+if (m.sender.startsWith('57' || '57')) {
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'reject')}
 
-if (m.sender.startsWith('90')) {
+if (m.sender.startsWith('90' || '90')) {
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'reject')}
 
-if (m.sender.startsWith('212')) {
+if (m.sender.startsWith('212' || '212')) {
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'reject')}
 
-if (m.sender.startsWith('92')) {
+if (m.sender.startsWith('92' || '92')) {
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'reject')}
 
-if (m.sender.startsWith('93')) {
+if (m.sender.startsWith('93' || '93')) {
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'reject')}
 
-if (m.sender.startsWith('94')) {
+if (m.sender.startsWith('94' || '94')) {
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'reject')}
 
-if (m.sender.startsWith('7')) {
+if (m.sender.startsWith('7' || '7')) {
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'reject')}
 
-if (m.sender.startsWith('49')) {
+if (m.sender.startsWith('49' || '49')) {
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'reject')}
 
-if (m.sender.startsWith('2')) {
+if (m.sender.startsWith('2' || '2')) {
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'reject')}
 
-if (m.sender.startsWith('91')) {
+if (m.sender.startsWith('91' || '91')) {
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'reject')}
 
-if (m.sender.startsWith('48')) {
+if (m.sender.startsWith('48' || '48')) {
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'reject')} 
 }}
 export default handler
