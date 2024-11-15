0b3e80a6e30cfc6b9f1a957106793c7e45269ab7
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-14 21:04:27 -0500

    Actualizar AutoRechazar.js

--- plugins/AutoRechazar.js
+++ plugins/AutoRechazar.js
@@ -6,18 +6,14 @@ let chat = global.db.data.chats[m.chat]
 if (chat.autoRechazar && !isAdmin) {
     if (!isBotAdmin) return !0
         const participants = await conn.groupRequestParticipantsList(m.chat)
-        const rawUser = m.messageStubParameters[0];
-const users = rawUser.split('@')[0]; 
-const prefijosProhibidos = ['91', '57', '92', '222', '93', '265', '61', '62', '966', '229', '40', '49', '20', '963', '967', '234', '210', '212'];
-const usersConPrefijo = users.startsWith('+') ? users : `+${users}`;
-        if (prefijosProhibidos.some(prefijo => usersConPrefijo.startsWith(prefijo))) {
+        const antiprefix = '91', '57', '92', '222', '93', '265', '61', '62', '966', '229', '40', '49', '20', '963', '967', '234', '210', '212'
+        const filteredParticipants = participants.filter(p => p.jid.includes('@s.whatsapp.net') && p.jid.split('@')[0].startsWith(antiprefix))
+        for (const participant of filteredParticipants) {
             await conn.groupRequestParticipantsUpdate(m.chat, [participant.jid], "reject")
-conn.reply('573012482597@s.whatsapp.net', 'ya', m)
         }
         if (m.messageStubType === 172 && m.messageStubParameters) {
             const [jid] = m.messageStubParameters
-            if (prefijosProhibidos.some(prefijo => usersConPrefijo.startsWith(prefijo))) {
-conn.reply('573012482597@s.whatsapp.net', 'ya', m)
+            if (jid.includes('@s.whatsapp.net') && jid.split('@')[0].startsWith(antiprefix)) {
                 await conn.groupRequestParticipantsUpdate(m.chat, [jid], "reject")}}
 }}
 export default handler
