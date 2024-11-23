413b75d4da7fa2b1cfa81e3a3b2da76e687525ab
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-23 17:10:30 -0500

    🦋 Hutao Proyect ❤️‍🔥

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -1,13 +1,37 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw '⚠ *_️Ingrese el error ue desea reportar._*'
-    if (text.length < 10) throw '⚠️ *_Especifique bien el error, mínimo 10 caracteres._*'
-    if (text.length > 1000) throw '⚠️ *_Máximo 1000 caracteres para enviar el error._*'
-    const teks = `╭───────────────────\n│⊷〘 *R E P O R T E* 🤍 〙⊷\n├───────────────────\n│⁖🧡꙰  *Cliente:*\n│✏️ Wa.me/${m.sender.split`@`[0]}\n│\n│⁖💚꙰  *Mensaje:*\n│📩 ${text}\n╰───────────────────`
-    await conn.reply(global.owner[0][0] + '@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
-    m.reply('⚠️ *_El reporte se envío a mi creador, cualquier informe falso puede ocasionar baneo._*')
+    if (!text) return conn.reply(m.chat, '⚠ *Ingrese el error que desea reportar.*', m)
+    if (text.length < 10) return conn.reply(m.chat, '⚠️ *Especifique bien el error, mínimo 10 caracteres.*', m)
+    let perfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://qu.ax/QGAVS.jpg')
+    if (text.length > 1000) return conn.reply(m.chat, '⚠️ *Máximo 1000 caracteres para enviar el error.*', m)
+    const teks = `*❌️ \`R E P O R T E\` ❌️*
+
+☁️ Número:
+• Wa.me/${m.sender.split`@`[0]}
+
+👤 Usuario: 
+• ${m.pushName || 'Anónimo'}
+
+💬 Mensaje:
+• ${text}`
+  ///  await conn.reply('573012482597@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
+
+  await conn.reply(global.owner[0][0] + '@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
+
+await conn.sendMessage(global.idchannel, { text: m.quoted ? teks + m.quoted.text : teks, contextInfo: {
+externalAdReply: {
+title: "【 🔔 𝐄𝐑𝐑𝐎𝐑 🔔 】",
+body: '💤 ¡𝚄𝚗 𝚞𝚜𝚞𝚊𝚛𝚒𝚘 𝚑𝚊 𝚛𝚎𝚙𝚘𝚛𝚝𝚊𝚍𝚘 𝚞𝚗 𝚎𝚛𝚛𝚘𝚛!',
+thumbnailUrl: perfil,
+sourceUrl: redes,
+mediaType: 1,
+showAdAttribution: false,
+renderLargerThumbnail: false
+}}}, { quoted: null })
+
+    m.reply('⚠️ *El reporte se envío a mi creador, cualquier informe falso puede ocasionar baneo.*')
 }
 handler.help = ['reportar']
 handler.tags = ['info']
-handler.command = ['reporte','report','reportar','bug','error']
+handler.command = ['reporte', 'report', 'reportar', 'bug', 'error']
 
 export default handler
