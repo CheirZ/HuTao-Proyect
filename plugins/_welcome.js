602b3e3e1c9a8f5fb26a388c3e8790d75786b231
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2024-11-29 22:44:23 -0600

    🦋 HuTao-Proyect ❤️‍🔥

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,44 +1,50 @@
-import {WAMessageStubType} from '@whiskeysockets/baileys'
-import fetch from 'node-fetch'
+let WAMessageStubType = (await import('@whiskeysockets/baileys')).default;
+import fetch from 'node-fetch';
 
-export async function before(m, {conn, participants, groupMetadata}) {
-  if (!m.messageStubType || !m.isGroup) return !0;
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://ibb.co/WBYqRDd')
-  let img = await (await fetch(`${pp}`)).buffer()
-  let chat = global.db.data.chats[m.chat]
-  
+export async function before(m, { conn, participants, groupMetadata }) {
+  if (!m.messageStubType || !m.isGroup) return true;
+
+  let vn = 'https://qu.ax/Jinc.mp3';
+  let vn2 = 'https://qu.ax/ujpr.mp3';
+  let chat = global.db.data.chats[m.chat];
+  const getMentionedJid = () => {
+    return m.messageStubParameters.map(param => `${param}@s.whatsapp.net`);
+  };
+
   let who = m.messageStubParameters[0] + '@s.whatsapp.net';
   let user = global.db.data.users[who];
 
   let userName = user ? user.name : await conn.getName(who);
 
-  if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `*⭒─ׄ─ׅ─ׄ─⭒ Welcome User ⭒─ׄ─ׅ─ׄ─⭒*
-    
-╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*
-┊ ‹‹ *Welcome* : *@${m.messageStubParameters[0].split`@`[0]}⁩*
-┊•*⁀➷ °⭒⭒⭒ *【 ✯ ${groupMetadata.subject} ✰ 】*
-╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩`;
-    
-await conn.sendMini(m.chat, packname, textbot,channel, estilo)
+ if (chat.welcome && m.messageStubType === 27) {
+    this.sendMessage(m.chat, { audio: { url: vn }, 
+    contextInfo: { forwardedNewsletterMessageInfo: { 
+    newsletterJid: channelRD.id, 
+    serverMessageId: '', 
+    newsletterName: channelRD.name }, forwardingScore: 9999999, isForwarded: true, mentionedJid: getMentionedJid(), "externalAdReply": { 
+    "title":`♡︎✿︎𝙱𝚒𝚎𝚗𝚟𝚎𝚗𝚒𝚍𝚘ꨄ︎ఌ︎`, 
+    "body": `${userName}`, 
+    "previewType": "PHOTO", 
+    "thumbnailUrl": null,
+    "thumbnail": icons, 
+    "sourceUrl": redes, 
+    "showAdAttribution": true}}, 
+     seconds: '4556', ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: fkontak, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
+}
+
+  if (chat.welcome && (m.messageStubType === 28 || m.messageStubType === 32)) {
+    this.sendMessage(m.chat, { audio: { url: vn2 }, 
+    contextInfo: { forwardedNewsletterMessageInfo: { 
+    newsletterJid: channelRD.id, 
+    serverMessageId: '', 
+    newsletterName: channelRD.name }, forwardingScore: 9999999, isForwarded: true, mentionedJid: getMentionedJid(), "externalAdReply": { 
+    "title": `❀☹︎𝙰𝚍𝚒𝚘𝚜☹︎☯︎`, 
+    "body": `${userName}, se despide.`, 
+    "previewType": "PHOTO", 
+    "thumbnailUrl": null,
+    "thumbnail": icons, 
+    "sourceUrl": redes, 
+    "showAdAttribution": true}}, 
+     seconds: '4556', ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: fkontak, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
   }
-  
-  if (chat.welcome && m.messageStubType == 28) {
-    let bye = `*⭒─ׄ─ׅ─ׄ─⭒ Bye User ⭒─ׄ─ׅ─ׄ─⭒*
-    
-╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*
-┊ ‹‹ *Bye* : *@${m.messageStubParameters[0].split`@`[0]}⁩*
-┊•*⁀➷ °⭒⭒⭒ *【 ✯ ${groupMetadata.subject} ✰ 】*
-╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩`;
-await conn.sendMini(m.chat, packname, textbot, channel, estilo)
-  }
-  
-  if (chat.welcome && m.messageStubType == 32) {
-    let kick = `*⭒─ׄ─ׅ─ׄ─⭒ Adios Put@ ⭒─ׄ─ׅ─ׄ─⭒*
-    
-╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*
-┊ ‹‹ *Pendej@ fuera* : *@${m.messageStubParameters[0].split`@`[0]}⁩*
-┊•*⁀➷ °⭒⭒⭒ *【 ✯ ${groupMetadata.subject} ✰ 】*
-╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩`;
-await conn.sendMini(m.chat, packname, textbot, channel, estilo)
-}}
+}
