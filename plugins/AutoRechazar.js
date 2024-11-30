1754719daf7aeb0936dead96f36a739411f631fa
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-11-30 10:17:23 -0500

    🦋 Hutao Proyect ❤️‍🔥

--- plugins/AutoRechazar.js
+++ plugins/AutoRechazar.js
@@ -8,7 +8,7 @@ if (isBotAdmin && chat.autoRechazar) {
 const prefixes = ['52', '90', '963', '966', '967', '249', '212', '92', '93', '94', '7', '49', '2', '91', '48']
 if (prefixes.some(prefix => m.sender.startsWith(prefix))) {
 await conn.groupRequestParticipantsUpdate(m.chat, [m.sender], 'reject')
-await conn.reply(idchannel, 'Un usuario fue rechazado de un grupo, por incumplir el prefijo, su numero es arabe el cual se cancela la solicitud de ingreso', null)
+return await conn.reply(idchannel, 'Un usuario fue rechazado de un grupo, por incumplir el prefijo, su numero es arabe el cual se cancela la solicitud de ingreso', null)
 }}}
 
 export default handler
