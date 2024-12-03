83feddd760f6f09e5cd16260380385cbca9ab8b9
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2024-12-03 13:42:58 -0600

    🦋 HuTao-Proyect ❤️‍🔥

--- plugins/anime-imagenes.js
+++ plugins/anime-imagenes.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const handler = async (m, {command, conn, usedPrefix}) => {
-const res = (await axios.get(`https://raw.githubusercontent.com/WOTCHITA/YaemoriBot-MD/master/src/JSON/anime-${command}.json`)).data;
+const res = (await axios.get(`https://raw.githubusercontent.com/CheirZ/HuTao-Proyect/master/src/JSON/anime-${command}.json`)).data;
 const haha = await res[Math.floor(res.length * Math.random())];
 conn.sendFile(m.chat, haha, 'error.jpg', `𖦹 *${command}*`, m, null, rcanal);
 };
