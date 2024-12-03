4f1f005d18ccf123e12d11142a039106d37b213e
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2024-12-03 00:02:21 -0600

    Update jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -72,7 +72,7 @@ return resultado;
 const message = users.map((v, index) => `• 「 ${index + 1} 」\n📎 Wa.me/${v.user.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}estado\n👤 Usuario: ${v.user.name || 'Sub-Bot'}\n🕑 Online: ${ v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : 'Desconocido'}`).join('\n\n__________________________\n\n');
 const replyMessage = message.length === 0 ? `No hay Sub-Bots disponible por el momento, verifique mas tarde.` : message;
 const totalUsers = users.length;
-const responseMessage = `🌻 *LISTA DE SUBBOTS*\n\n⭐️ PUEDES PEDIR PERMISO PARA QUE TE DEJEN UNIR EL BOT A TÚ GRUPO\n\n\`\`\`CADA USUARIO SUB BOT USA FUNCIÓN COMO QUIERA, EL NÚMERO PRINCIPAL NO SE HACE RESPONSABLE DEL USO DE LA FUNCIÓN \`\`\`\n\nSUBBOT CONECTADO: ${totalUsers || '0'}\n\n${replyMessage.trim()}`.trim();
+const responseMessage = `❤️‍🔥 *ᎪϘႮᏆ ͲᏆᎬΝᎬՏ ᏞᎪ ᏞᏆՏͲᎪ ᎠᎬ ՏႮᏴ ᏴϴͲՏ*\n\n🦋 habla con el propietario del sub bot y pidele permiso de entrar a tu grupo\n\n\`\`\`Cada sub bot es diferente y cada usuario/propietario usa el sub como quiera, El equipo de HuTao-Proyect no se hace responsable del uso que le den al mismo sub-bot \`\`\`\n\nSUBBOT CONECTADO: ${totalUsers || '0'}\n\n${replyMessage.trim()}`.trim();
 await _envio.sendMessage(m.chat, {text: responseMessage, mentions: _envio.parseMention(responseMessage)}, {quoted: m})
 break   
 }}
