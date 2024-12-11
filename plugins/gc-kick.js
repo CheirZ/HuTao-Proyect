e6edec1a10b86480989b18db120dbd85c2bd0abd
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-11 17:50:41 -0500

    Gg

--- plugins/gc-kick.js
+++ plugins/gc-kick.js
@@ -1,4 +1,7 @@
 var handler = async (m, { conn, participants, usedPrefix, command }) => {
+    if (!m.mentionedJid[0] && !m.quoted) {
+        return conn.reply(m.chat, '🚩 *Etiqueta o responde al mensaje de la persona que quieres eliminar*', m, rcanal );
+    }
 
     let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
 
@@ -7,16 +10,28 @@ var handler = async (m, { conn, participants, usedPrefix, command }) => {
     const ownerBot = global.owner[0][0] + '@s.whatsapp.net';
     //const nn = conn.getName(m.sender);
 
-   await conn.groupParticipantsUpdate("120363292605251530@g.us", "51987041650", 'remove');
+    if (user === conn.user.jid) {
+        return conn.reply(m.chat, '🚩 No puedo eliminar el bot del grupo', m, rcanal, );
+    }
+
+    if (user === ownerGroup) {
+        return conn.reply(m.chat, '🚩 No puedo eliminar al propietario del grupo', m, rcanal, );
+    }
+
+    if (user === ownerBot) {
+        return conn.reply(m.chat, '🚩 No puedo eliminar al propietario del bot', m, rcanal );
+    }
+
+    await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
 
 };
 
 handler.help = ['kick'];
 handler.tags = ['grupo'];
 handler.command = ['kick','echar','hechar','sacar','ban'];
-//handler.admin = true;
-//handler.group = true;
+handler.admin = true;
+handler.group = true;
 //handler.register = false
-//handler.botAdmin = true;
+handler.botAdmin = true;
 
 export default handler;
