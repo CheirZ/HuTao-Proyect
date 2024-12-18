cf76be5fc8c099e3c844e5aa8fb5be9074b02743
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-18 08:17:01 -0500

    Actualizar owner-enviarmsg.js

--- plugins/owner-enviarmsg.js
+++ plugins/owner-enviarmsg.js
@@ -21,22 +21,22 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     let content = m.quoted ? m.quoted : m;
     let messageOptions = {};
 
-    if (content.message.imageMessage) {
-        messageOptions = { image: content.message.imageMessage, caption: content.message.imageMessage.caption || '' };
-    } else if (content.message.videoMessage) {
-        messageOptions = { video: content.message.videoMessage, caption: content.message.videoMessage.caption || '' };
-    } else if (content.message.stickerMessage) {
-        messageOptions = { sticker: content.message.stickerMessage };
-    } else if (content.message.documentMessage) {
-        messageOptions = { document: content.message.documentMessage, fileName: content.message.documentMessage.fileName };
+    if (content.message?.imageMessage) {
+        messageOptions = { image: { url: content.message.imageMessage.url }, caption: content.message.imageMessage.caption || '' };
+    } else if (content.message?.videoMessage) {
+        messageOptions = { video: { url: content.message.videoMessage.url }, caption: content.message.videoMessage.caption || '' };
+    } else if (content.message?.stickerMessage) {
+        messageOptions = { sticker: { url: content.message.stickerMessage.url } };
+    } else if (content.message?.documentMessage) {
+        messageOptions = { document: { url: content.message.documentMessage.url }, fileName: content.message.documentMessage.fileName };
     } else {
-        messageOptions = { text: text || content.message.conversation || content.message.extendedTextMessage.text };
+        messageOptions = { text: text || content.message?.conversation || content.message?.extendedTextMessage?.text };
     }
 
     await conn.sendMessage(idgroup, messageOptions)
         .then(() => {
-           // let senderInfo = `Mensaje enviado por @${who.split('@')[0]}`;
-           // conn.sendMessage(idgroup, { text: senderInfo, mentions: [who] });
+            //let senderInfo = `Mensaje enviado por @${who.split('@')[0]}`;
+            //conn.sendMessage(idgroup, { text: senderInfo, mentions: [who] });
         })
         .catch(err => {
             console.error('Error al enviar el mensaje:', err);
