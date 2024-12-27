da74c963e7fba2c5b2c61bf9fe1d2bd07f2ca530
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-27 18:13:29 -0500

    Actualizar gc-setpp.js

--- plugins/gc-setpp.js
+++ plugins/gc-setpp.js
@@ -1,15 +1,30 @@
-import { makeWASocket } from '@whiskeysockets/baileys';
+import { downloadContentFromMessage } from '@whiskeysockets/baileys';
+import { writeFileSync, unlinkSync } from 'fs';
+import path from 'path';
 
-let handler = async (m, { conn, usedPrefix, command }) => {
+let handler = async (m, { conn }) => {
+
   let q = m.quoted ? m.quoted : m;
-  let mime = (q.msg || q).mimetype || q.mediaType || '';
+  let mime = (q.message || q).mimetype || q.mediaType || '';
 
   if (/image/.test(mime)) {
-    let img = await q.download();
-    if (!img) return m.reply('「✦」 Por favor, responde a una imagen válida.');
+    try {
+      const stream = await downloadContentFromMessage(q.message.imageMessage, 'image');
+      let buffer = Buffer.from([]);
 
-    await conn.updateProfilePicture(m.chat, img);
-    return m.reply('「✦」 La foto de perfil del grupo se ha cambiado exitosamente.');
+      for await (const chunk of stream) {
+        buffer = Buffer.concat([buffer, chunk]);
+      }
+
+      const filePath = path.join(__dirname, 'temp-image.jpg');
+      writeFileSync(filePath, buffer);
+
+      await conn.updateProfilePicture(m.chat, { url: filePath });
+      unlinkSync(filePath); // Eliminar el archivo después de actualizar la imagen del grupo ._., Hutao hace momos en este archivo.
+      return m.reply('「✦」 La foto de perfil del grupo se ha cambiado exitosamente.');
+    } catch (e) {
+      return m.reply(`「✦」 Hubo un error al actualizar la imagen: ${e.message}`);
+    }
   } else {
     return m.reply('「✦」 Por favor, responde a una imagen válida.');
   }
