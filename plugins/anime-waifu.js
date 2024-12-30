d3ba3f7dfe67e025604fe423b9ea631f663a1194
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-30 14:20:44 -0500

    Actualizar anime-waifu.js

--- plugins/anime-waifu.js
+++ plugins/anime-waifu.js
@@ -1,53 +1,32 @@
-import fetch from 'node-fetch'
+const handler = async (m, { conn }) => {
+  const taguser = '@' + m.sender.split('@')[0]; // Obtiene el usuario etiquetado
 
-let handler = async (m, { conn }) => {
-    try {
-        await m.react(emojis)
-        conn.reply(m.chat, 'Ꙭ Buscando Su *Waifu*', m, {
-            contextInfo: {
-                externalAdReply: {
-                    mediaUrl: null,
-                    mediaType: 1,
-                    showAdAttribution: true,
-                    title: packname,
-                    body: wm,
-                    previewType: 0,
-                    thumbnail: icons,
-                    sourceUrl: channel
-                }
-            }
-        })
+  conn.sendMessage(m.chat, {
+    image: { url: 'https://qu.ax/MFOVJ.jpg' },
+    caption: `You like me? ${taguser}`, // Mención visible del usuario
+    footer: "Sock",
+    buttons: [
+      {
+        buttonId: ".gay",
+        buttonText: {
+          displayText: "Yes",
+        },
+        type: 1,
+      },
+      {
+        buttonId: ".play2 felices los 4",
+        buttonText: {
+          displayText: "No",
+        },
+        type: 1,
+      },
+    ],
+    viewOnce: true,
+    headerType: 4,
+    mentions: [m.sender], // Mención funcional
+  }, { quoted: m });
+};
 
-        let res = await fetch('https://api.waifu.pics/sfw/waifu')
-        if (!res.ok) return
-        let json = await res.json()
-        if (!json.url) return
+handler.command = /^(test3)$/i; // Define el comando
 
-        let buttons = [
-            {buttonId: ".menu", buttonText: {displayText: 'Menu'}, type: 1},
-            {buttonId: ".waifu", buttonText: {displayText: "Siguiente"}, type: 1}
-        ]
-
-        let buttonMessage = {
-            image: { url: json.url },
-            caption: `★ *W A I F U* ★`,
-            footer: wm,
-            buttons: buttons,
-            headerType: 4,
-            viewOnce: true,
-            mentions: [m.sender]
-        }
-
-        await conn.sendMessage(m.chat, buttonMessage, { quoted: m })
-    } catch (e) {
-        console.error(e)
-        conn.reply(m.chat, 'Hubo un error al intentar enviar el mensaje.\n\n> ' + e, m)
-    }
-}
-
-handler.help = ['waifu']
-handler.tags = ['anime']
-handler.command = ['waifu']
-handler.group = true
-handler.register = false
-export default handler
+export default handler;
