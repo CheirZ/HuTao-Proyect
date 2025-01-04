94ba952d03f834286700361e6cf235fb8c624d56
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2025-01-04 07:18:18 -0600

    🦋 HuTao-Proyect ❤️‍🔥

--- plugins/inspectador.js
+++ plugins/inspectador.js
@@ -496,7 +496,6 @@ break
 handler.tags = ['tools']
 handler.help = ['nuevafotochannel', 'nosilenciarcanal', 'silenciarcanal', 'noseguircanal', 'seguircanal', 'avisoschannel', 'resiviravisos', 'inspect', 'inspeccionar', 'eliminarfotochannel', 'reactioneschannel', 'reaccioneschannel', 'nuevonombrecanal', 'nuevadescchannel']
 handler.command = ['nuevafotochannel', 'nosilenciarcanal', 'silenciarcanal', 'noseguircanal', 'seguircanal', 'avisoschannel', 'resiviravisos', 'inspect', 'inspeccionar', 'eliminarfotochannel', 'reactioneschannel', 'reaccioneschannel', 'nuevonombrecanal', 'nuevadescchannel']
-handler.register = true
 export default handler 
 
 function formatDate(n, locale = "es", includeTime = true) {
