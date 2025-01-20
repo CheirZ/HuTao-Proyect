62f94b49c81193f1e7afb79151d4c2e22c152d97
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2025-01-19 22:29:20 -0600

    🦋 HuTao-Proyect ❤️‍🔥

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -3,6 +3,7 @@ let handler = async (m, { conn, command }) => {
 let grupos =  `Hola, 
 *➤ 𝙶𝚛𝚞𝚙𝚘𝚜 donde puedes encontrar el bot y hablar con amigos*
 *1.-* https://chat.whatsapp.com/KxHaM2J0NWPDR4RU24OmFw
+*2.-* https://chat.whatsapp.com/JD5t9cd2mzr5eLMLbSr2lF
 `
 await m.react('❤️‍🔥')
 await conn.sendFile(m.chat, imagen1, "hutao.jpg", grupos, fkontak, null, rcanal)}
