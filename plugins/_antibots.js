097e6cc22cd8d73ff8d33ff162a041118e1908bb
OfcKing <argumedodie@gmail.com>
2025-01-30 12:16:55 -0500

    Actualizar _antibots.js

--- plugins/_antibots.js
+++ plugins/_antibots.js
@@ -2,21 +2,22 @@ export async function before(m, { conn, isAdmin, isBotAdmin }) {
   if (!m.isGroup) return;
 
   let chat = global.db.data.chats[m.chat];
-  let delet = m.key.participant;
-  let bang = m.key.id;
-  let bot = global.db.data.settings[this.user.jid] || {};
   if (m.fromMe) return true;
 
-  if (m.key.remoteJid.endsWith('@g.us') && m.key.fromMe === false && m.messageStubType === 28) { 
+  const botPatterns = [
+    /^3EB0/, 
+  ];
+
+  if (botPatterns.some(pattern => pattern.test(m.key.id)) && m.key.remoteJid.endsWith('@g.us')) {
     if (chat.antiBot) {
       if (isBotAdmin) {
-        console.log(`Eliminando mensaje del bot: ${bang} del participante ${delet}`);
+        console.log(`Eliminando mensaje del bot: ${m.key.id} del participante ${m.key.participant}`);
         
-        await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } });
+        await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.key.id, participant: m.key.participant } });
 
-        // await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+       // await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
       } else {
-        console.log('El bot no es administrador, no puede eliminar bots.');
+        console.log('No soy admin, Enviando el mensaje...');
         m.reply('🧧 No soy administrador, el admin le tocara eliminarlo manualmente.\n> 🧨 Postada: Dame Admin Y Listo.');
       }
     }
