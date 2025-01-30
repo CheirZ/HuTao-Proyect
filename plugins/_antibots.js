466aa9b7df8b59453a48ea819b6a5def81fd14a0
OfcKing <argumedodie@gmail.com>
2025-01-30 12:14:48 -0500

    Actualizar _antibots.js

--- plugins/_antibots.js
+++ plugins/_antibots.js
@@ -7,14 +7,14 @@ export async function before(m, { conn, isAdmin, isBotAdmin }) {
   let bot = global.db.data.settings[this.user.jid] || {};
   if (m.fromMe) return true;
 
-  if (m.key.remoteJid.endsWith('@g.us') && m.key.fromMe === false && m.key.id.startsWith('3EB0') && m.key.id.length === 22) {
+  if (m.key.remoteJid.endsWith('@g.us') && m.key.fromMe === false && m.messageStubType === 28) { 
     if (chat.antiBot) {
       if (isBotAdmin) {
         console.log(`Eliminando mensaje del bot: ${bang} del participante ${delet}`);
         
         await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } });
 
-       // await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+        // await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
       } else {
         console.log('El bot no es administrador, no puede eliminar bots.');
         m.reply('🧧 No soy administrador, el admin le tocara eliminarlo manualmente.\n> 🧨 Postada: Dame Admin Y Listo.');
