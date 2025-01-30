9157c1741dcb652f624ef18be1c0ec604dd4fe90
OfcKing <argumedodie@gmail.com>
2025-01-30 12:03:10 -0500

    Actualizar _antibots.js

--- plugins/_antibots.js
+++ plugins/_antibots.js
@@ -7,11 +7,11 @@ export async function before(m, { conn, isAdmin, isBotAdmin }) {
   let bot = global.db.data.settings[this.user.jid] || {};
   if (m.fromMe) return true;
 
-  if (m.key.id.startsWith('BAE5') && m.key.id.length === 16) { 
+  if (m.key.remoteJid.endsWith('@g.us') && m.key.fromMe === false && m.messageStubType === 68) {
     if (chat.antiBot) {
       if (isBotAdmin) {
         await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }});
-        await m.reply('🧧 Un Bot.');
+        await m.reply('🧧 Un Bot');
        // await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
       } else {
         m.reply('🧧 No soy administrador, el admin le tocara eliminarlo manualmente.\n> 🧨 Postada: Dame Admin Y Listo.');
