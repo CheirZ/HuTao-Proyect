5bab673201247edea8b63ba6420b84e0c0ff067e
OfcKing <argumedodie@gmail.com>
2025-01-30 12:19:44 -0500

    No sirvw

--- plugins/_antibots.js
+++ plugins/_antibots.js
@@ -1,4 +1,4 @@
-export async function before(m, { conn, isAdmin, isBotAdmin }) {
+/*export async function before(m, { conn, isAdmin, isBotAdmin }) {
   if (!m.isGroup) return;
 
   let chat = global.db.data.chats[m.chat];
@@ -60,4 +60,4 @@ export async function before(m, { conn, isAdmin, isBotAdmin }) {
       }
     }
   }
-}
+}*/
