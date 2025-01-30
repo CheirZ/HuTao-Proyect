d7b9ccc277b8abb9f8d91691da44235f3493b9b9
OfcKing <argumedodie@gmail.com>
2025-01-30 12:04:44 -0500

    Actualizar _antibots.js

--- plugins/_antibots.js
+++ plugins/_antibots.js
@@ -1,21 +1,19 @@
 export async function before(m, { conn, isAdmin, isBotAdmin }) {
-  if (!m.isGroup) return;
+    if (!m.isGroup) return;
+    let chat = global.db.data.chats[m.chat]
+    let delet = m.key.participant
+    let bang = m.key.id
+    let bot = global.db.data.settings[this.user.jid] || {}
+    if (m.fromMe) return true;
 
-  let chat = global.db.data.chats[m.chat];
-  let delet = m.key.participant;
-  let bang = m.key.id;
-  let bot = global.db.data.settings[this.user.jid] || {};
-  if (m.fromMe) return true;
+    if (m.id.startsWith('3EB0') && m.id.length === 22) {
+        let chat = global.db.data.chats[m.chat];
 
-  if (m.key.remoteJid.endsWith('@g.us') && m.key.fromMe === false && m.messageStubType === 68) {
-    if (chat.antiBot) {
-      if (isBotAdmin) {
-        await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }});
-        await m.reply('🧧 Un Bot');
-       // await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-      } else {
-        m.reply('🧧 No soy administrador, el admin le tocara eliminarlo manualmente.\n> 🧨 Postada: Dame Admin Y Listo.');
-      }
+        if (chat.antiBot) {
+            if (isBotAdmin) {
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+//await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+            }
+        }
     }
-  }
 }
