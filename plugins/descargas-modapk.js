72e94fe365e879bd4ac2246165a494f0a397f1a5
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2025-02-14 06:20:16 -0600

    🦋 HuTao-Proyect ❤️‍🔥

--- plugins/descargas-modapk.js
+++ plugins/descargas-modapk.js
@@ -4,7 +4,7 @@ var handler = async (m, {conn, usedPrefix, command, text}) => {
 if (!text) return conn.reply(m.chat, '💥 *Ingrese el nombre de la apk para descargarlo.*', m, rcanal)
 try {
 await m.react(rwait)
-conn.reply(m.chat, '💥 *Descargando su aplicación...*', m,), /*{
+conn.reply(m.chat, '💥 *Descargando su aplicación...*', m, rcanal), /*{
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: dev,
