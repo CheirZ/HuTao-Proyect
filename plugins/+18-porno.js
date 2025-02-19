5340a21294e5b5f19c796dcb1a73dc8465bf0637
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2025-02-18 23:17:30 -0600

    🦋 HuTao-Proyect ❤️‍🔥

--- plugins/+18-porno.js
+++ plugins/+18-porno.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const handler = async (m, {command, conn, usedPrefix}) => {
 if (!db.data.chats[m.chat].nsfw && m.isGroup) return m.reply('🚩 *¡Estos comandos están desactivados!*');    
-conn.reply(m.chat, `❤️‍🔥 *Enviando ${command}...*`, m)}; /*{
+conn.reply(m.chat, `❤️‍🔥 *Enviando ${command}...*`, m); /*{
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: wm,
