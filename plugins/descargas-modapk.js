15d36efcc4c348b7e441d35d858afb60d250f8ae
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2025-02-18 23:27:54 -0600

    🦋 HuTao-Proyect ❤️‍🔥

--- plugins/descargas-modapk.js
+++ plugins/descargas-modapk.js
@@ -4,7 +4,7 @@ var handler = async (m, {conn, usedPrefix, command, text}) => {
 if (!text) return conn.reply(m.chat, '💥 *Ingrese el nombre de la apk para descargarlo.*', m, rcanal)
 try {
 await m.react(rwait)
-conn.reply(m.chat, '💥 *Descargando su aplicación...*', m, rcanal),
+conn.reply(m.chat, '💥 *Descargando su aplicación...*', m)
 let searchA = await search(text)
 let data5 = await download(searchA[0].id)
 let txt = `*乂  APTOIDE - DESCARGAS* 乂\n\n`
@@ -12,7 +12,7 @@ txt += `💥 *Nombre* : ${data5.name}\n`
 txt += `🗃 *Package* : ${data5.package}\n`
 txt += `🪴 *Update* : ${data5.lastup}\n`
 txt += `⚖ *Peso* :  ${data5.size}`
-await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m),
+await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m)
 await m.react(done)  
 if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
 return await conn.reply(m.chat, '🛑 *El archivo es demaciado pesado*', m, rcanal)}
