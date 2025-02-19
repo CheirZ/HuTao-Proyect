41983732adb0b2b4862c457c97185baf9ae64b9b
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2025-02-18 23:48:01 -0600

    🦋 HuTao-Proyect ❤️‍🔥

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -110,7 +110,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
         `https://api.siputzx.my.id/api/d/ytmp4?url=${url}`,
         `https://api.zenkey.my.id/api/download/ytmp4?apikey=zenkey&url=${url}`,
         `https://axeel.my.id/api/download/video?url=${encodeURIComponent(url)}`,
-        `https://delirius-apiofc.vercel.app/download/ytmp4?url=${url}`
+        `https://delirius-apiofc.vercel.app/download/ytmp4v2?url=${url}`
       ];
 
       let success = false;
