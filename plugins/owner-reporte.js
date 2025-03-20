a6b274f6d3df1cd98c6538034849f4d73eefe323
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2025-03-20 16:33:40 -0600

    🦋 HuTao-Proyect ❤️‍🔥

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) return conn.reply(m.chat, '⚠ *Ingrese el error que desea reportar.*', m)
     if (text.length < 10) return conn.reply(m.chat, '⚠️ *Especifique bien el error, mínimo 10 caracteres.*', m)
-    let perfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://qu.ax/QGAVS.jpg')
+    let perfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://cdn.donmai.us/original/b0/0a/__hu_tao_and_boo_tao_genshin_impact_drawn_by_jidou_unorihi__b00aad2420afad5245b49a260b2eef82.jpg')
     if (text.length > 1000) return conn.reply(m.chat, '⚠️ *Máximo 1000 caracteres para enviar el error.*', m)
     const teks = `*❌️ \`R E P O R T E\` ❌️*
 
@@ -17,7 +17,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
   await conn.reply(global.owner[0][0] + '@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
 
-await conn.sendMessage(global.idchannel, { text: m.quoted ? teks + m.quoted.text : teks, contextInfo: {
+/*await conn.sendMessage(global.idchannel, { text: m.quoted ? teks + m.quoted.text : teks, contextInfo: {
 externalAdReply: {
 title: "【 🔔 𝐄𝐑𝐑𝐎𝐑 🔔 】",
 body: '💤 ¡𝚄𝚗 𝚞𝚜𝚞𝚊𝚛𝚒𝚘 𝚑𝚊 𝚛𝚎𝚙𝚘𝚛𝚝𝚊𝚍𝚘 𝚞𝚗 𝚎𝚛𝚛𝚘𝚛!',
@@ -26,7 +26,7 @@ sourceUrl: redes,
 mediaType: 1,
 showAdAttribution: false,
 renderLargerThumbnail: false
-}}}, { quoted: null })
+}}}, { quoted: null })*/
 
     m.reply('⚠️ *El reporte se envío a mi creador, cualquier informe falso puede ocasionar baneo.*')
 }
