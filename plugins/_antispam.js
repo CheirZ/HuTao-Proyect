e7cf752aaf7831f2e241e6b4fd9a962786958094
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2025-03-26 19:08:59 -0600

    🦋 HuTao-Proyect ❤️‍🔥

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -1,12 +1,12 @@
 const userSpamData = {}
 let handler = m => m
-handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner, isPrems}) {
+handler.before = async function (m, {conn, isAdmin, isBotAdmin}) {
 const chat = global.db.data.chats[m.chat]
 const bot = global.db.data.settings[conn.user.jid] || {}
 if (!bot.antiSpam) return
 if (m.isGroup && chat.modoadmin) return  
 if (m.isGroup) {
-if (isOwner || isROwner || isAdmin || !isBotAdmin || isPrems) return
+if (isAdmin || !isBotAdmin) return
 }  
 let user = global.db.data.users[m.sender]
 const sender = m.sender
