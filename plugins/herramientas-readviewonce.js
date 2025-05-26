8cb4ebf71f24dc9e39a2173b1096118d8a465b0e
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2025-05-26 13:20:42 -0600

    🦋 HuTao-Proyect ❤️‍🔥

--- plugins/herramientas-readviewonce.js
+++ plugins/herramientas-readviewonce.js
@@ -1,8 +1,8 @@
 let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
 
 let handler = async (m, { conn }) => {
-if (!m.quoted) return conn.reply(m.chat, `${e} Responde a una imagen ViewOnce.`, m)
-if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `${e} Responde a una imagen ViewOnce.`, m)
+if (!m.quoted) return conn.reply(m.chat, `Responde a una imagen ViewOnce.`, m)
+if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `Responde a una imagen ViewOnce.`, m, fkontak)
 let buffer = await m.quoted.download(false);
 if (/videoMessage/.test(m.quoted.mtype)) {
 return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', m)
