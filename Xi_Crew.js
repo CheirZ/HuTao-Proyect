0b61f698f6fc11683d06322099be9f0636d776b8
Alex-X <argumedodie@gmail.com>
2025-06-05 19:28:32 -0500

    Actualizar Xi_Crew.js

--- Xi_Crew.js
+++ Xi_Crew.js
@@ -133,7 +133,7 @@ return msg?.message || ""
 msgRetryCounterCache, // Resolver mensajes en espera
 msgRetryCounterMap, // Determinar si se debe volver a intentar enviar un mensaje o no
 defaultQueryTimeoutMs: undefined,
-version: [2, 3000, 1023223821] // Posdata: quiten la opción QR
+version: [2, 3000, 1023223821], // Posdata: quiten la opción QR
 }
 
 global.conn = makeWASocket(connectionOptions);
