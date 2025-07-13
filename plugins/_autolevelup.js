731e3103b9a7a3a6e822e958eabcd05065449860
Alex-X <argumedodie@gmail.com>
2025-07-13 14:51:20 -0500

    Actualizar _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -24,19 +24,6 @@ if (before !== user.level) {
 let currentRole = Object.entries(roles).sort((a, b) => b[1] - a[1]) .find(([, minLevel]) => level + 1 >= minLevel)[0]
 let nextRole = Object.entries(roles).sort((a, b) => a[1] - b[1]) .find(([, minLevel]) => level + 2 < minLevel)[0]
 
-//if (user.role != currentRole && level >= 1) {
-if (level >= 1) {
-user.role = currentRole
-let text22 = `✨ ¡𝙵𝚎𝚕𝚒𝚌𝚒𝚍𝚊𝚍𝚎𝚜 *${userName}*, 𝚙𝚘𝚛 𝚝𝚞 𝚗𝚞𝚎𝚟𝚘 𝚛𝚊𝚗𝚐𝚘!\n\n\`𝙽𝚞𝚎𝚟𝚘 𝚁𝚊𝚗𝚐𝚘:\`\n${currentRole}`
-if (nextRole) {
-text22 += `\n\n> 𝙿𝚛𝚘́𝚡𝚒𝚖𝚘 𝚛𝚊𝚗𝚐𝚘 ${nextRole}, 𝚎𝚗 𝚎𝚕 *𝚗𝚒𝚟𝚎𝚕 ${roles[nextRole]}*. ¡𝚂𝚒𝚐𝚞𝚎 𝚊𝚜𝚒!`
-}
-
-m.reply(text22)
-}
-
-m.reply(`*🎉 ¡ F E L I C I D A D E S ! 🎉*\n\n💫 Nivel Actual » *${user.level}*\n🌵 Rango » ${user.role}\n📆 Fecha » *${moment.tz('America/Bogota').format('DD/MM/YY')}*\n\n> *\`¡Has alcanzado un Nuevo Nivel!\`*`)
-
 let especial = 'moras'
 let especial2 = 'exp'
 let especial3 = 'money'
@@ -55,234 +42,121 @@ let normalCant = [1, 3, 3, 3, 4, 4, 2, 2, 4, 4, 4, 4, 1].getRandom()
 let normalCant2 = [1, 3, 2, 2, 4, 4, 2, 2, 4, 4, 5, 5, 1].getRandom() 
 let normalCant3 = [1, 3, 3, 3, 4, 4, 2, 2, 4, 4, 4, 4, 1].getRandom() 
 
-if (level >= 1) {
-let texto22 = `👤 *𝚄𝚜𝚞𝚊𝚛𝚒𝚘:* ${userName}\n🐢 *𝙽𝚒𝚟𝚎𝚕 𝚊𝚗𝚝𝚎𝚛𝚒𝚘𝚛:* ${before}\n⭐️ *𝙽𝚒𝚟𝚎𝚕 𝚊𝚌𝚝𝚞𝚊𝚕:* ${level + 1}\n👾 *𝚁𝚊𝚗𝚐𝚘:* ${user.role}${(level + 1) % 5 === 0 ? `\n\n💰 *𝚁𝚎𝚌𝚘𝚖𝚙𝚎𝚗𝚜𝚊 𝚙𝚘𝚛 𝚊𝚕𝚊𝚌𝚊𝚗𝚣𝚊𝚛 𝚎𝚕 𝚗𝚒𝚟𝚎𝚕 ${level + 1}:*
-🎁 *Bono:* \`X${Math.floor(((level + 1) - 5) / 10) + 1}\`
-- *${especialCant * (Math.floor(((level + 1) - 5) / 10) + 1)} 🪙 ${especial}*
-- *${especialCant2 * (Math.floor(((level + 1) - 5) / 10) + 1)} ✨️ ${especial2}*
-- *${especialCant3 * (Math.floor(((level + 1) - 5) / 10) + 1)} 💸 ${especial3}*
-- *${especialCant4 * (Math.floor(((level + 1) - 5) / 10) + 1)} 🪙 ${especial4}*
-
-> 👀 Siguiente recompensa en el *nivel ${level + 6}*` : ''}`.trim()
-
-m.reply(texto22)
-}
-
 if (user.level == 5){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 5!!* 🏆
-ᰔᩚ *${especialCant * 1} ${especial}*
-ᰔᩚ *${especialCant2 * 1} ${especial2}*
-ᰔᩚ *${especialCant3 * 1} ${especial3}*
-ᰔᩚ *${especialCant4 * 1} ${especial4}*`, m)
 user[especial] += especialCant * 1
 user[especial2] += especialCant2 * 1
 user[especial3] += especialCant3 * 1
 user[especial4] += especialCant4 * 1
 
 }else if (user.level == 10){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 10!!* 🏆
-ᰔᩚ *${especialCant * 1} ${especial}*
-ᰔᩚ *${especialCant2 * 1} ${especial2}*
-ᰔᩚ *${especialCant3 * 1} ${especial3}*
-ᰔᩚ *${especialCant4 * 1} ${especial4}*`, m)
 user[especial] += especialCant * 1
 user[especial2] += especialCant2 * 1
 user[especial3] += especialCant3 * 1
 user[especial4] += especialCant4 * 1
 
 }else if (user.level == 15){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 15!!* 🏆
-ᰔᩚ *${especialCant * 2} ${especial}*
-ᰔᩚ *${especialCant2 * 2} ${especial2}*
-ᰔᩚ *${especialCant3 * 2} ${especial3}*
-ᰔᩚ *${especialCant4 * 2} ${especial4}*`, m)
 user[especial] += especialCant * 2
 user[especial2] += especialCant2 * 2
 user[especial3] += especialCant3 * 2
 user[especial4] += especialCant4 * 2
 
 }else if (user.level == 20){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 20!!* 🏆
-ᰔᩚ *${especialCant * 2} ${especial}*
-ᰔᩚ *${especialCant2 * 2} ${especial2}*
-ᰔᩚ *${especialCant3 * 2} ${especial3}*
-ᰔᩚ *${especialCant4 * 2} ${especial4}*`, m)
 user[especial] += especialCant * 2
 user[especial2] += especialCant2 * 2
 user[especial3] += especialCant3 * 2
 user[especial4] += especialCant4 * 2
 
 }else if (user.level == 25){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 25!!* 🏆
-ᰔᩚ *${especialCant * 3} ${especial}*
-ᰔᩚ *${especialCant2 * 3} ${especial2}*
-ᰔᩚ *${especialCant3 * 3} ${especial3}*
-ᰔᩚ *${especialCant4 * 3} ${especial4}*`, m)
 user[especial] += especialCant * 3
 user[especial2] += especialCant2 * 3
 user[especial3] += especialCant3 * 3
 user[especial4] += especialCant4 * 3
 
 }else if (user.level == 30){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 30!!* 🏆
-ᰔᩚ *${especialCant * 3} ${especial}*
-ᰔᩚ *${especialCant2 * 3} ${especial2}*
-ᰔᩚ *${especialCant3 * 3} ${especial3}*
-ᰔᩚ *${especialCant4 * 3} ${especial4}*`, m)
 user[especial] += especialCant * 3
 user[especial2] += especialCant2 * 3
 user[especial3] += especialCant3 * 3
 user[especial4] += especialCant4 * 3
 
 }else if (user.level == 35){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 35!!* 🏆
-ᰔᩚ *${especialCant * 4} ${especial}*
-ᰔᩚ *${especialCant2 * 4} ${especial2}*
-ᰔᩚ *${especialCant3 * 4} ${especial3}*
-ᰔᩚ *${especialCant4 * 4} ${especial4}*`, m)
 user[especial] += especialCant * 4
 user[especial2] += especialCant2 * 4
 user[especial3] += especialCant3 * 4
 user[especial4] += especialCant4 * 4
 
 }else if (user.level == 40){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 40!!* 🏆
-ᰔᩚ *${especialCant * 4} ${especial}*
-ᰔᩚ *${especialCant2 * 4} ${especial2}*
-ᰔᩚ *${especialCant3 * 4} ${especial3}*
-ᰔᩚ *${especialCant4 * 4} ${especial4}*`, m)
 user[especial] += especialCant * 4
 user[especial2] += especialCant2 * 4
 user[especial3] += especialCant3 * 4
 user[especial4] += especialCant4 * 4
 
 }else if (user.level == 45){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 45!!* 🏆
-ᰔᩚ *${especialCant * 5} ${especial}*
-ᰔᩚ *${especialCant2 * 5} ${especial2}*
-ᰔᩚ *${especialCant3 * 5} ${especial3}*
-ᰔᩚ *${especialCant4 * 5} ${especial4}*`, m)
 user[especial] += especialCant * 5
 user[especial2] += especialCant2 * 5
 user[especial3] += especialCant3 * 5
 user[especial4] += especialCant4 * 5
 
 }else if (user.level == 50){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 50!!* 🏆
-ᰔᩚ *${especialCant * 5} ${especial}*
-ᰔᩚ *${especialCant2 * 5} ${especial2}*
-ᰔᩚ *${especialCant3 * 5} ${especial3}*
-ᰔᩚ *${especialCant4 * 5} ${especial4}*`, m)
 user[especial] += especialCant * 5
 user[especial2] += especialCant2 * 5
 user[especial3] += especialCant3 * 5
 user[especial4] += especialCant4 * 5
 
 }else if (user.level == 55){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 55!!* 🏆
-ᰔᩚ *${especialCant * 6} ${especial}*
-ᰔᩚ *${especialCant2 * 6} ${especial2}*
-ᰔᩚ *${especialCant3 * 6} ${especial3}*
-ᰔᩚ *${especialCant4 * 6} ${especial4}*`, m)
 user[especial] += especialCant * 6
 user[especial2] += especialCant2 * 6
 user[especial3] += especialCant3 * 6
 user[especial4] += especialCant4 * 6
 
 }else if (user.level == 60){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 60!!* 🏆
-ᰔᩚ *${especialCant * 6} ${especial}*
-ᰔᩚ *${especialCant2 * 6} ${especial2}*
-ᰔᩚ *${especialCant3 * 6} ${especial3}*
-ᰔᩚ *${especialCant4 * 6} ${especial4}*`, m)
 user[especial] += especialCant * 6
 user[especial2] += especialCant2 * 6
 user[especial3] += especialCant3 * 6
 user[especial4] += especialCant4 * 6
 
 }else if (user.level == 65){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 65!!* 🏆
-ᰔᩚ *${especialCant * 7} ${especial}*
-ᰔᩚ *${especialCant2 * 7} ${especial2}*
-ᰔᩚ *${especialCant3 * 7} ${especial3}*
-ᰔᩚ *${especialCant4 * 7} ${especial4}*`, m)
 user[especial] += especialCant * 7
 user[especial2] += especialCant2 * 7
 user[especial3] += especialCant3 * 7
 user[especial4] += especialCant4 * 7
 
 }else if (user.level == 70){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 70!!* 🏆
-ᰔᩚ *${especialCant * 7} ${especial}*
-ᰔᩚ *${especialCant2 * 7} ${especial2}*
-ᰔᩚ *${especialCant3 * 7} ${especial3}*
-ᰔᩚ *${especialCant4 * 7} ${especial4}*`, m)
 user[especial] += especialCant * 7
 user[especial2] += especialCant2 * 7
 user[especial3] += especialCant3 * 7
 user[especial4] += especialCant4 * 7
 
-}else if (user.level == 75){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 75!!* 🏆
-ᰔᩚ *${especialCant * 8} ${especial}*
-ᰔᩚ *${especialCant2 * 8} ${especial2}*
-ᰔᩚ *${especialCant3 * 8} ${especial3}*
-ᰔᩚ *${especialCant4 * 8} ${especial4}*`, m) 
+}else if (user.level == 75){ 
 user[especial] += especialCant * 8
 user[especial2] += especialCant2 * 8
 user[especial3] += especialCant3 * 8
 user[especial4] += especialCant4 * 8
 
 }else if (user.level == 80){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 80!!* 🏆
-ᰔᩚ *${especialCant * 8} ${especial}*
-ᰔᩚ *${especialCant2 * 8} ${especial2}*
-ᰔᩚ *${especialCant3 * 8} ${especial3}*
-ᰔᩚ *${especialCant4 * 8} ${especial4}*`, m)
 user[especial] += especialCant * 8
 user[especial2] += especialCant2 * 8
 user[especial3] += especialCant3 * 8
 user[especial4] += especialCant4 * 8
 
 }else if (user.level == 85){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 85!!* 🏆
-ᰔᩚ *${especialCant * 9} ${especial}*
-ᰔᩚ *${especialCant2 * 9} ${especial2}*
-ᰔᩚ *${especialCant3 * 9} ${especial3}*
-ᰔᩚ *${especialCant4 * 9} ${especial4}*`, m)
 user[especial] += especialCant * 9
 user[especial2] += especialCant2 * 9
 user[especial3] += especialCant3 * 9
 user[especial4] += especialCant4 * 9
 
 }else if (user.level == 90){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 90!!* 🏆
-ᰔᩚ *${especialCant * 9} ${especial}*
-ᰔᩚ *${especialCant2 * 9} ${especial2}*
-ᰔᩚ *${especialCant3 * 9} ${especial3}*
-ᰔᩚ *${especialCant4 * 9} ${especial4}*`, m)
 user[especial] += especialCant * 9
 user[especial2] += especialCant2 * 9
 user[especial3] += especialCant3 * 9
 user[especial4] += especialCant4 * 9
 
 }else if (user.level == 95){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 95!!* 🏆
-ᰔᩚ *${especialCant * 10} ${especial}*
-ᰔᩚ *${especialCant2 * 10} ${especial2}*
-ᰔᩚ *${especialCant3 * 10} ${especial3}*
-ᰔᩚ *${especialCant4 * 10} ${especial4}*`, m)
 user[especial] += especialCant * 10
 user[especial2] += especialCant2 * 10
 user[especial3] += especialCant3 * 10
 user[especial4] += especialCant4 * 10
 
 }else if (user.level == 100){
-conn.reply(m.chat, `*🥳 RECOMPENSA POR SU NUEVO NIVEL 100!!* 🏆
-ᰔᩚ *${especialCant * 10} ${especial}*
-ᰔᩚ *${especialCant2 * 10} ${especial2}*
-ᰔᩚ *${especialCant3 * 10} ${especial3}*
-ᰔᩚ *${especialCant4 * 10} ${especial4}*`, m)
 user[especial] += especialCant * 10
 user[especial2] += especialCant2 * 10
 user[especial3] += especialCant3 * 10
