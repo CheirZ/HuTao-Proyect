a17e85389ef54872505675b622c98e93f10e404f
Alex-X <argumedodie@gmail.com>
2025-07-14 10:14:01 -0500

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -1,71 +1,9 @@
-import { startSubBot } from '../lib/subs.js';
+const handler = async (m, { start: conn }) => {
 
-const commandFlags = {};
-
-const generateCaption = (isCode, devContact = '✿') => {
-  if (isCode) {
-    return `☯ sᴜʙ ʙᴏᴛ-ᴍᴏᴅᴇ ᴄᴏᴅᴇ
-
-✰ Usa éste Código para convertirte en Sub-Bot Temporal.
-
-→ Tres Puntitos
-→ Dispositivos Vinculados
-→ Vincular Dispositivo
-→ Vincular con el número de teléfono.
-
-➤ *Importante:*
-» No es recomendable usar tu cuenta principal.
-
-${devContact}`;
-  }
-
-  return `↝↣☬ʜᴜᴛᴀᴏ-ᴘʀᴏʏᴇᴄᴛ֍↜↤
-
-ↂ SUB BOT FUNCION֎
-
-*❤️‍🩹 Usa otro cel o tu PC para vincular el bot en el dispositivo que será el SubBot*
-
-\`1\` » Haz clic en los 3 puntitos de la parte superior derecha
-\`2\` » Toca en dispositivos vinculados
-\`3\` » Escanea el código QR para iniciar sesión con el bot
-❤️‍🔥 *Este código QR expira en 45 segundos*
-
-*𝐉𝐀𝐃𝐈𝐁𝐎𝐓 𝐄𝐃𝐈𝐓𝐀𝐃𝐎 𝐏𝐎𝐑 𝐗𝐢_𝐌𝐢𝐠𝐮𝐞𝐥𝐨𝐧77𝐗𝐗*`;
-};
-
-const handler = async (m, { conn, command }) => {
-  const phone = m.sender.split('@')[0];
-  const isCode = command?.toLowerCase() === 'code';
-  const caption = generateCaption(isCode, global.dev);
-
-  if (!m.sender || !phone) {
-    return conn.sendMessage(m.chat, {
-      text: '[ ✿ ] No se pudo procesar tu solicitud. El identificador del usuario es inválido.',
+    return start.sendMessage(m.chat, {
+      text: '[ ✿ ] Cómando en mantenimiento....',
       quoted: m
     });
-  }
-
-  if (commandFlags[m.sender]) {
-    return conn.sendMessage(m.chat, {
-      text: '[ ✿ ] Ya estás solicitando un SubBot. Espera unos segundos antes de volver a intentarlo.',
-      quoted: m
-    });
-  }
-
-  commandFlags[m.sender] = true;
-
-  try {
-    const result = await startSubBot(m, conn, caption, isCode, phone, m.chat, commandFlags);
-
-  } catch (err) {
-    console.error('Error iniciando SubBot:', err);
-    await conn.sendMessage(m.chat, {
-      text: '[ ✿ ] Hubo un error al iniciar el SubBot. Inténtalo nuevamente más tarde.',
-      quoted: m
-    });
-  } finally {
-    setTimeout(() => delete commandFlags[m.sender], 90000);
-  }
 };
 
 handler.help = ['qr', 'code'];
