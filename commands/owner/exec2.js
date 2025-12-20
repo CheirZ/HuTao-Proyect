83d53f295a2170c9b7c9ef3ec4cfd1ede38808c0
Ell Sarmiento 🦩 <sarmientodieg@gmail.com>
2025-12-19 22:07:41 -0500

    Actualizar exec2.js

--- commands/owner/exec2.js
+++ commands/owner/exec2.js
@@ -1,24 +1,27 @@
-import cp, { exec as _exec } from 'child_process'
+import cp from 'child_process'
 import { promisify } from 'util'
+const exec = promisify(cp.exec)
 
-const exec = promisify(_exec).bind(cp)
 export default {
   command: ['r'],
   isOwner: true,
-  run: async (client, m, args, command, text) => {
-  if (!global.db.data.settings[client.user.id.split(':')[0] + '@s.whatsapp.net']?.owner) return
-    let o
+  run: async (client, m, args) => {
+    const cmd = args.join(' ').trim()
+    if (!cmd) {
+      return // client.reply(m.chat, '❌ Escribe un comando para ejecutar.\nEjemplo: .r ls', m)
+    }
+    let result
     try {
-      // await m.react('🕒')
-      o = await exec(command.trimStart() + ' ' + text.trimEnd())
-      // await m.react('✔️')
+      result = await exec(cmd)
     } catch (e) {
-      o = e
-     // await m.react('✖️')
-    } finally {
-     const { stdout, stderr } = o
-      if (stdout?.trim()) client.reply(m.chat, stdout, m)
-      if (stderr?.trim()) client.reply(m.chat, stderr, m)
-    }  
+      result = e
+    }
+    const { stdout, stderr } = result
+    if (stdout?.trim()) {
+      client.reply(m.chat, stdout.trim(), m)
+    }
+    if (stderr?.trim()) {
+      client.reply(m.chat, stderr.trim(), m)
+    }
   }
 }
