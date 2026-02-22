fd7baf6a4da942d7e22e38de920ed103d3b9e8eb
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2026-02-21 22:57:01 -0600

    🦋 HuTao-Proyect ❤️‍🔥

--- commands/dow/fb.js
+++ commands/dow/fb.js
@@ -12,7 +12,7 @@ export default {
     }
 
     try {
-      const videoUrl = `https://api.evogb.org/dl/facebook2?url=${args[0]}&key=proyectsV2`
+      const videoUrl = ` ${api.url}/dl/facebook?url=${args[0]}&key=${api.key} `
 
       const response = await fetch(videoUrl)
       if (!response.ok) throw new Error(`HTTP ${response.status}`)
