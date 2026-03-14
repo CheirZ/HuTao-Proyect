ac1eb223d1ede79e4979ed3cd245d8e3a6fe7bb6
ɪ'ᴍ ᴅɪᴇɢᴏ ᜊ•ᴗ•ᜊ <sarmientodieg@gmail.com>
2026-03-14 18:34:44 -0500

    Actualizar subbot.js

--- commands/socket/subbot.js
+++ commands/socket/subbot.js
@@ -1,4 +1,4 @@
-import { startSubBot } from '../../lib/subs.ts';
+import { startSubBot } from '../../lib/subs.js';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url'
