88a3758f30b4e0b3eb6155ff1f8184f6a403f587
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2026-03-15 20:27:47 -0600

    🦋 HuTao-Proyect ❤️‍🔥

--- commands/dow/instagram.js
+++ commands/dow/instagram.js
@@ -20,7 +20,7 @@ export default {
         const medias = []
         for (const url of urls.slice(0, 10)) {
           try {
-            const res = await fetch(`${api.url}/dl/instagram?url=${encodeURIComponent(url)}&key=${api.key}`)
+            const res = await fetch(`${api.url2}/dl/instagram?url=${encodeURIComponent(url)}&key=${api.key2}`)
             const json = await res.json()
             if (!json.status || !json.data) continue
 
