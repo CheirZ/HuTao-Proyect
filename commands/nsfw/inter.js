67a8e9f3040608c2f523d63570253ca907796f41
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2026-03-15 20:40:45 -0600

    🦋 HuTao-Proyect ❤️‍🔥

--- commands/nsfw/inter.js
+++ commands/nsfw/inter.js
@@ -66,7 +66,7 @@ export default {
 
     try {
       const response = await fetch(
-        `${api.url2}/nsfw/interaction?type=${currentCommand}&key=${api.key2}`,
+        `${api.url}/nsfw/interaction?type=${currentCommand}&key=${api.key}`,
       )
       const json = await response.json()
       const { result } = json
