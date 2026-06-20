63444acc423ea10a0fc83b3c526d255d16dfd6a0
I'am Diego <sarmientodieg@gmail.com>
2026-06-20 17:22:25 -0500

    Actualizar rw.js

--- cmds/gacha/rw.js
+++ cmds/gacha/rw.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 import fetch from 'node-fetch';
 
 const obtenerImagen = async (keyword) => {
-  const endpoints = ["gelbooru", "safebooru", "danbooru"];
+  const endpoints = ["safebooru", "gelbooru", "danbooru"];
 
   for (const endpoint of endpoints) {
     try {
